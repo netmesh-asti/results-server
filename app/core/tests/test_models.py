87e4a1e1ab606e201f6a13d47bc973f75757fea8
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2022-04-08 13:52:20 +0800

    fix PEP8 errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,19 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class testUserModel(TestCase):
 
     def test_create_user_success(self):
+        """Test that normal user is created"""
         email = 'test@gmail.com'
         password = 'testpassword123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
-        self.assertEqual(user.email, email )
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_create_superuser_success(self):
+        """Test that super user is created"""
         email = 'test@gmail.com'
         password = 'testpassword123'
         user = get_user_model().objects.create_superuser(
@@ -25,8 +28,13 @@ class testUserModel(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_serialized(self):
-        """Test that the new user email is normalize"""
+        """Test that the new user email is normalize (lowercase afer @)"""
         email = 'test@gmaIl.coM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpassword123')
