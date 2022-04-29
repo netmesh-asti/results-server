7e2df30eed6814b3c4930da2e9cfd06920f46c04
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2022-04-29 11:29:10 +0800

    Created admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('first_name','last_name')}),
+        (
+            _('Permissions'),
+            {'fields':('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Affiliation'), {'fields': ('is_ntc',)}),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.RFC6349TestDevice)
+admin.site.register(models.FieldTester)
+admin.site.register(models.Test)
+admin.site.register(models.DataPoint)
