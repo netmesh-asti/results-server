a994d4bbbe34626483633828abbaafc2eb0ba7db
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2022-07-11 12:22:55 +0800

    Removed user/me/ endpoint. user/account/ is used instead.

--- app/user/urls.py
+++ app/user/urls.py
@@ -6,7 +6,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('me/', views.ManageUserView.as_view(), name='me'),
     path('account/', views.ManageFieldUsersView.as_view(), name='account'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
 
