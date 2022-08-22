fb75c300eb1d4c8b17a30d192d694fa2b3c6c653
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2022-08-22 16:45:26 +0800

    changed user endpoint paths

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.ListUsersView.as_view(), name='users'),
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('myprofile/', views.ManageUserView.as_view(), name='my-profile'),
-    path('manage/', views.ManageFieldUsersView.as_view(), name='manage-account'),
+    path('manage/', views.ManageFieldUsersView.as_view(),
+         name='manage-account'),
     path('token/', views.AuthTokenView.as_view(), name='token')
 ]
