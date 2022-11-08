71be92d1c499d837aa970bec80143724000d2c53
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2022-11-08 18:20:36 +0800

    Create user set active serializers and API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'nro',
             'profile_picture',
-            'is_staff')
+            'is_staff',)
         read_only_fields = ('id', 'profile_picture', 'nro', 'is_staff')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
         depth = 1
@@ -99,3 +99,13 @@ class ProfileImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'profile_picture']
         read_only_fields = ['id']
         extra_kwargs = {'profile_picture': {'required': 'True'}}
+
+
+class UserActiveSerializer(serializers.ModelSerializer):
+    """Serializer for uploading profile pictures to Users."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'is_active']
+        read_only_fields = ['id']
+        extra_kwargs = {'is_active': {'required': 'True'}}
